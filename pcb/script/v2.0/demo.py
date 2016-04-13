1e47e6960c1027e664a663c5f493d59181fff733
PengPeng <516190948@qq.com>
2016-04-13 15:06:56 +0800

    完善pyautocad脚本

--- pcb/script/v2.0/demo.py
+++ pcb/script/v2.0/demo.py
@@ -10,10 +10,10 @@ def demo():
     print("文件名:") 
     print(acad.doc.Name) 
     p1 = APoint(0, 0)
-    p2 = APoint(5000, 2500)
+    p2 = APoint(50, 25)
     acad.model.AddLine(p1, p2)
-    acad.model.AddCircle(p1, 1000)
+    acad.model.AddCircle(p1, 55)
+    print("绘制完成.") 
 
 if __name__ == '__main__': 
     demo()
-    print("ok")
