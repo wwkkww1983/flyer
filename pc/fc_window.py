b720b695bb5a1be5dbbbab0e79e6b0cfb12785fc
PengPeng <5616190948@qq.com>
2016-07-02 20:30:23 +0800

    搭建上位机新框架

---
+++ pc/fc_window.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QLabel
+
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.uic import loadUiType, loadUi
+
+FCWindowUIClass = loadUiType("fc_window.ui")
+
+class FCWindow(QMainWindow):
+    def __init__(self):
+        super(QMainWindow, self).__init__() 
+
+        # 初始化UI
+        self.mUi = FCWindowUIClass[0]()
+        self.mUi.setupUi(self)
+
+        # 菜单在ui文件中初始化
+        # 此处指初始化状态栏
+        statusLabel1 = QLabel("状态栏占位标签")
+        self.mStatusBar = self.mUi.statusbar
+        self.mStatusBar.addWidget(statusLabel1)
+
+        # 标题
+        self.setWindowTitle("飞控上位机")
+
+if __name__ == '__main__': 
+    app = QApplication(sys.argv)
+    win = FCWindow()
+    win.show()
+    sys.exit(app.exec_())
+
--- pc/main.pyw
+++ pc/main.pyw
@@ -8,14 +8,28 @@ opengl:pip install PyOpenGL(PyOpenGL3.1.0)
 """
 
 import sys
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMessageBox
 
-from fc_widgets import FCWindow
+from fc_window import FCWindow
 
-app = QtWidgets.QApplication(sys.argv)
-win= FCWindow()
-win.move(0,0)
-win.show()
-sys.exit(app.exec_())
+try:
+    from OpenGL import GL
+except ImportError: 
+    app = QApplication(sys.argv)
+    QMessageBox.critical(None, "飞控上位机环境", 
+    """
+    需要以下环境
+    os:win7 64
+    pyqt:PyQt5-5.6-gpl-Py3.5-Qt5.6.0-x64-2
+    opengl:pip install PyOpenGL(PyOpenGL3.1.0)
+    """)
+    sys.exit(1) 
 
+if __name__ == '__main__': 
+    app = QApplication(sys.argv)
+    win = FCWindow()
+    win.move(0,0)
+    win.show()
+    sys.exit(app.exec_())
 
