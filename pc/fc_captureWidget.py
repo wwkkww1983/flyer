1a702fd3d4fe7345f1c00ef36f87356922804e06
PengPeng <516190948@qq.com>
2016-07-22 12:20:47 +0800

    实现上位机采样请求帧

--- pc/fc_captureWidget.py
+++ pc/fc_captureWidget.py
@@ -12,16 +12,17 @@ from PyQt5.QtCore import pyqtSignal
 from PyQt5.uic import loadUiType, loadUi
 
 from fc_waveWidget import FCWaveWidget
+from fc_frame import FCFrame
 
 FCWindowUIClass = loadUiType("fc_captureWidget.ui")
 
 class FCCaptureWidget(QWidget):
     def __init__(self):
         super(FCCaptureWidget, self).__init__() 
-        # 正在捕获
+        # 通信线程&串口&标记
         self.mCapturing = False
-        self.mRecvThread = threading.Thread(target=self.RecvFunc)
-        self.mRecvThread.daemon = True # 主线程结束 子线程也结束
+        self.mSerial = None
+        self.mRecvThread = None
 
         # 初始化UI
         self.mUi = FCWindowUIClass[0]()
@@ -57,35 +58,71 @@ class FCCaptureWidget(QWidget):
     def ChangeState(self, checked):
         if self.mCapturing:
             self.mCapturing = False 
+            self.StopCapture()
             self.mCapturePushButton.setText("开始采集") 
             self.mCommGroupBox.setEnabled(True)
             self.mDataGroupBox.setEnabled(True)
-            self.StopCapture()
         else:
             self.mCapturing = True
+            self.StartCapture()
             self.mCapturePushButton.setText("停止采集")
             self.mCommGroupBox.setEnabled(False)
             self.mDataGroupBox.setEnabled(False)
-            self.StartCapture()
 
     def StartCapture(self):
         # step1: 启动串口线程
+        self.mSerial = Serial()
+        comPort = self.mComNameComboBox.currentText()
+        comBaudrate = self.mBuadLineEdit.text()
+        self.mSerial.port = comPort
+        self.mSerial.baudrate = comBaudrate
+        self.mSerial.timeout = 0 #非阻塞调用
+        self.mSerial.open()
+        self.mRecvThread = threading.Thread(target=self.RecvFunc)
+        self.mRecvThread.daemon = True # 主线程结束 子线程也结束
         self.mRecvThread.start()
-        # step2: 组帧
+
+        # step2: 组请求帧
+        # TODO: 需要根据控制界面定制
+        time = int(self.mIntervalLineEdit.text())
+        #print(time)
+        frameType = b'\x02\x00\x00\x03'
+        frameLen = 16
+        frameData = time
+
+        frame = FCFrame(frameType, frameLen, frameData)
+        buf = frame.GetBytes()
+        print(buf)
+
         # step3: 发帧
+        self.mSerial.write(buf)
+
         print("开始采集")
 
     def StopCapture(self):
-        # step1: 组帧
+        # step1: 组停止帧
+
         # step2: 发帧
+
         # step3: 停止串口线程
         self.mRecvThread.join(10) #等待10s
+        self.mSerial.close()
+        self.mSerial = None
+        self.mRecvThread = None
         print("停止采集")
 
     def RecvFunc(self):
+        line_nums = 0;
+        #line_bytes = ser.read(g_frame_size)
         while self.mCapturing:
-            print("RecvFunc")
-            sleep(1)
+            #line = self.mSerial.readline()
+            line = self.mSerial.read(1)
+            if 0 != len(line): 
+                line_nums = line_nums + 1 
+                line_str = line
+                #line_str = line.decode('utf8') 
+                print(line_nums, end = ':')
+                print(line_str)
 
     def ChangeCommType(self, typeIndex):
         if 0 != typeIndex: 
--- pc/fc_frame.py
+++ pc/fc_frame.py
@@ -2,15 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import struct
+import binascii
+
+#self.mLen = struct.pack('>I', fLen) 
+#self.mData = struct.pack('>I', fData)
 
 class FCFrame():
-    def __init__(self):
+    def __init__(self, fType, fLen, fData):
         super(FCFrame, self).__init__() 
+        self.mType = fType
+        self.mLen = struct.pack('>I', fLen)
+        self.mData = struct.pack('>I', fData)
+        self.mCrc32 = None
 
-        self.mType = b'\x00\x00\x00\x00'
-        self.mLen = b'\x00\x00\x00\x00'
-        self.mData = b''
-        self.mCrc32 = b'\x00\x00\x00\x00'
+    def ComputeCrc32(self):
+        buf = self.mType + self.mLen + self.mData
+        #print(buf)
+        #print(len(buf) % 4)
+        if 0 != (len(buf) % 4):
+            print("不是4字节整数倍,出错.")
+            exit()
+        crc32 = (binascii.crc32(buf) & 0xffffffff)
+        self.mCrc32 = struct.pack('>i', crc32)
+        #print("0x%x" % crc32)
+        #print(self.mCrc32)
+
+    def GetBytes(self):
+        self.ComputeCrc32()
+        buf = self.mType + self.mLen + self.mData + self.mCrc32
+        return buf
 
     def Print(self):
         print("type: ", end = '')
@@ -21,7 +42,6 @@ class FCFrame():
         print(self.mData)
         print("crc32:", end = '')
         print(self.mCrc32)
-
 
 if __name__ == '__main__': 
     frame = FCFrame()
