6175e12e2560666bc233bc1bbdf6414c48cae309
PengPeng <516190948@qq.com>
2016-08-12 14:46:41 +0800

    完善上位机程序

--- pc/fc_waveWidget.py
+++ pc/fc_waveWidget.py
@@ -11,15 +11,24 @@ class FCWaveWidget(QWidget):
     #msMove = pyqtSignal(QPoint, name='msMove')
     msMove = pyqtSignal(['QPoint', 'int'], name = "msMove")
 
-    def __init__(self, parent=None):
-        super(FCWaveWidget, self).__init__(parent) 
+    def __init__(self):
+        super(FCWaveWidget, self).__init__() 
 
         #
         self.mConfig = {
-                '横坐标单位' : 'deg',
-                '纵坐标单位' : 'ms'
+                '横坐标单位' : '秒',
+                '横坐标步长' : 50,
+                '横坐标短线边界' : 8,
+
+                '纵坐标单位' : '  度  ',
+                '纵坐标步长' : 30,
+                '纵坐标短线边界' : 32,
                 }
         
+        # TODO: 使用参数传入
+        self.mXPhyStep = 100  #每个像素点为100ms间隔
+        #self.mYPhyStep = 0.1  #每个像素点为0.01deg间隔
+
         self.setMouseTracking(True) # 鼠标跟踪
         self.setCursor(Qt.BlankCursor) # 隐藏鼠标
         self.mPos = None # 鼠标当前点
@@ -31,19 +40,24 @@ class FCWaveWidget(QWidget):
         painter.fillRect(0, 0, self.width(), self.height(), Qt.black)
         # 绘制坐标系
         self.drawAxes(painter)
-        #self.drawAxes2(painter)
         # 绘制鼠标位置十字线
         self.drawMouseCross(painter)
 
         painter.end() 
 
-    def drawAxes2(self, painter): 
+    def drawAxes(self, painter): 
         xMax = self.width()
         yMax = self.height()
 
         metrics = painter.fontMetrics() 
         xUnit = self.mConfig['横坐标单位']
+        xStep = self.mConfig['横坐标步长']
+        xShortLineMagin = self.mConfig['横坐标短线边界']
+
         yUnit = self.mConfig['纵坐标单位']
+        yStep = self.mConfig['纵坐标步长']
+        yShortLineMagin = self.mConfig['纵坐标短线边界']
+
         #print(xUnit)
         #print(yUnit)
         leftWidth = metrics.width(yUnit)
@@ -57,65 +71,63 @@ class FCWaveWidget(QWidget):
         painter.setPen(pen)
 
         # 坐标原点
-        x = leftWidth
-        y = yMax - downHeight
-        painter.rotate(270)
-        painter.drawText(x, y, "O");
+        # +/-1避免被截断
+        x = leftWidth + 1
+        y = yMax - 1
+        #painter.rotate(270)
+        painter.drawText(x, y, "0");
 
         # 横坐标 
-        x = leftWidth
-        y = yMax - downHeight
-        painter.drawLine(x, y, xMax, y); 
-
-        # 画纵坐标 
-
-    def drawAxes(self, painter): 
-        metrics = painter.fontMetrics() 
-        
-        textHeight = metrics.ascent() + metrics.descent() 
-        leftWidth = metrics.width("V")
-        rightWidth = metrics.width("ms") 
-        width = leftWidth * 3
-        height = self.height() - textHeight  * 2
-
-        # 画坐标系
-        pen = QPen(Qt.red)
-        pen.setWidth(1)
-        painter.setPen(pen)
-        step = 50 # 间距50像素一个
-        lineLength = 5 # 线长
-        # 坐标原点
-        painter.drawText(width, height + lineLength * 3, "O");
-        # 画横坐标 
-        painter.drawLine(width, height, self.width() - 10, height); 
-        # 横坐标的标志点
-        length = self.width() - width - 10
-        shortLineNums = int(length / step)
-        #print(length)
-        #print(shortLineNums)
+        xStart = leftWidth
+        yStart = yMax - downHeight
+        painter.drawLine(xStart, yStart, xMax, yStart); 
+        # 坐标短线
+        length = xMax - xStart
+        shortLineNums = int(length / xStep) - 1 # -1 预留单位text输出的位置
+        shortLineLength = downHeight - xShortLineMagin
+        if shortLineLength < 0:
+            print("""短线边界过大,修改 = self.mConfig['短线边界']""")
+            return
         for i in range(0, shortLineNums):
-            x1 = (i + 1) * step 
+            x1 = (i + 1) * xStep + xStart
             x2 = x1
-            y1 = height
-            y2 = height + lineLength
+            y1 = yStart
+            y2 = yStart + shortLineLength
             painter.drawLine(x1, y1, x2, y2 )
-            text = "%d" % (i+1)
-            painter.drawText(x2 + 2, y2 + lineLength * 2, text)
+            text = "%.1f" % ((x1 - xStart) * self.mXPhyStep / 1000)
+            #print(text)
+            # +/-1 显示美观
+            painter.drawText(x2 + 1, yMax - 1, text)
+        painter.drawText(xMax - leftWidth, yMax - 1, xUnit)
 
         # 画纵坐标 
-        painter.drawLine(width, height, width, 10); 
-        length = height - 10
-        shortLineNums = int(length / step)
-        #print(length)
-        #print(shortLineNums)
+        xStart = leftWidth
+        yStart = yMax - downHeight
+        painter.drawLine(xStart, yStart, xStart, 0); 
+        # 坐标短线
+        length = yStart - 0 # 起点的y值为最大值
+        shortLineNums = int(length / yStep) - 1 # -1 预留单位text输出的位置
+        shortLineLength = leftWidth - yShortLineMagin
+        if shortLineLength < 0:
+            print("""短线边界过大,修改 = self.mConfig['短线边界']""")
+            return
+        # 计算纵向范围 
+        yRange = shortLineNums * yStep
+        # 每个像素代表的物理角度
+        yPhyPerPix = 360 / yRange
+        #print(yRange) 
+        #print(yPhyPerPix) 
         for i in range(0, shortLineNums):
-            x1 = width - lineLength
-            x2 = width
-            y1 = self.height() - ((i + 1) * step)
+            x1 = xStart
+            x2 = xStart - shortLineLength
+            y1 = yStart - (i + 1) * yStep
             y2 = y1
-            painter.drawLine(x1, y1, x2, y2 )
-            text = "%d" % (i+1)
-            painter.drawText(x2 - lineLength * 3 , y2 + 10, text)
+            painter.drawLine(x1, y1, x2, y2)
+            text = "%.1f" % ( ((i + 1 ) * yStep) * yPhyPerPix - 180.0)
+            #print(text)
+            # +/-1 显示美观
+            painter.drawText(0 + 1, y1 + 1, text)
+        painter.drawText(0 + 1, 0 + downHeight, yUnit)
 
     def drawMouseCross(self, painter): 
         if None != self.mPos:
