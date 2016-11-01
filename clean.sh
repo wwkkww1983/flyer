fab008b3e25fa8534075242cc8f51debdb4b995f
PengPeng <516190948@qq.com>
2016-11-01 15:36:55 +0800

    完善清理脚本

--- clean.sh
+++ clean.sh
@@ -9,7 +9,9 @@ rm -rf pc/__pycache__/
 rm -rf pc/bak/__pycache__/
 rm -rf pc/algo/__pycache__/
 rm -rf pc/frame/__pycache__/
+rm -rf pc/frame/data/__pycache__/
 rm -rf pc/comm/__pycache__/
+rm -rf pc/widget/__pycache__/
 #rm -rf pc/data/*
 
 
